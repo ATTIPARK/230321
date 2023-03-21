b4c3cd8d48e0b38e92b90c31c6d6d806b01c3af4
ATTIPARK <qkrqjawls0922@gmail.com>
2023-03-21 21:33:21 +0900

    Add Pikachu

---
+++ js/variables.js
@@ -0,0 +1,49 @@
+// const TRUE = true;
+// const FALSE = false;
+// && AND   피연산자가 둘다 참이면 true를 반환
+// console.log(TRUE && TRUE);
+// console.log(TRUE && FALSE);
+// console.log(FALSE && TRUE);
+// console.log(FALSE && FALSE);
+// console.log("======================================");
+// || OR    피연산자가 하나라도 참이면 true를 반환
+// console.log(TRUE || TRUE);
+// console.log(TRUE || FALSE);
+// console.log(FALSE || TRUE);
+// console.log(FALSE || FALSE);
+// console.log("======================================");
+// ! NOT    NOT이 붙으면 반대
+// console.log(!TRUE);
+// console.log(!FALSE);
+
+// let count = 5;
+// for (let n = 0; n <= count; n++) {
+//   for (let m = 0; m < n; m++) {
+//     console.lo("*");
+//   }
+//   console.log("");
+// }
+
+// var a = [1, 2, 3];
+// console.log(a);
+
+// const chains = [
+//   "mainnet",
+//   "polygon",
+//   "avalanche",
+//   "arbitrum",
+//   "gnosis",
+//   "bsc",
+//   "optimism",
+//   "fantom",
+// ];
+// console.log(chains);
+
+// const myProfile = {
+//   name: "Atti",
+//   job: "Programmer",
+//   age: 29,
+//   isKind: true,
+// };
+// const uglyMyProfile = ["atti", "Programmer", 29];
+// console.log(myProfile);
